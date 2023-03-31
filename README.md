137e6e5d884789a9b26f2f153171484cef44a60a
thepiguy <kunal.a.dandekar@gmail.com>
2023-03-31 17:33:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Inholland's database project rewritten in Rust
+
+I absolutely hate C#. I absolutely hate Windows and Winforms.
+Not only that, but I have not used windows in the past 4 years, and I would like to keep it that way.
+This means I need to spin up a slow shitty virtual machine every time I have to do this stupid project because of Winforms.
+So here is that project rewritten in Rust using the GTK toolkit.
+Plus, I will be using a simple PostgreSQL database hosted on my vps, because Azure SQL Server is also fucking dumb.
+
+This code is all licensed under the GNU AGPLv3 License.
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use adw::prelude::*;
+use adw::{glib, Application, ApplicationWindow};
+use gtk::Orientation;
+
+const APP_ID: &str = "me.piguy.inholland.someren";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+
+    return app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a button with label and margins
+    let button = gtk::Button::builder()
+        .label("Dashboard")
+        .build();
+
+    let content = gtk::Box::builder()
+        .orientation(Orientation::Vertical)
+        .build();
+
+    let stack = adw::ViewStack::builder().build();
+
+    stack.add_titled_with_icon(&gtk::Label::new(Some("Rooms")), Some("People"), "People", "money-clip-symbolic");
+    stack.add_titled_with_icon(&gtk::Label::new(Some("Rooms")), Some("Rooms"), "Rooms", "Page 2");
+    stack.add_titled_with_icon(&gtk::Label::new(Some("Activities")), Some("Activities"), "Activities", "Page 2");
+    stack.add_titled_with_icon(&gtk::Label::new(Some("Shop")), Some("Shop"), "Shop", "Page 2");
+
+    stack.set_vexpand(true);
+
+    let v = adw::ViewSwitcher::builder()
+        .policy(adw::ViewSwitcherPolicy::Wide)
+        .stack(&stack).build();
+
+    let header = adw::HeaderBar::builder().title_widget(&v)
+        .decoration_layout("icon:minimize,maximize,close")
+        .build();
+
+    header.pack_start(&button);
+
+    content.append(&header);
+    content.append(&stack);
+
+    let window = ApplicationWindow::builder()
+        .decorated(false)
+        .application(app)
+        .title("Someren")
+        .content(&content)
+        .build();
+
+    window.present();
+}
